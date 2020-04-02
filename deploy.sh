fdc36886c8c94cb110fac803e01865075e7e7a73
Tuan <admin@hungtuan.me>
2020-04-02 13:59:57 +0700

    edit ci/cd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker stop nodeapp
-docker rm nodeapp
 docker run -d -p 8888:8888 -e MONGODB_CONNECTION=mongodb://root:123@172.104.191.90/admin?retryWrites=true&w=majority  --name nodeapp tuannnh/nodejs-api
